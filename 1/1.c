0dbf30a6699bf123bf85b4510066fcabaedff174
YGAR84 <a.lyamin@g.nsu.ru>
2019-09-12 17:43:48 +0700

    comments for 1st lab

--- 1/1.c
+++ 1/1.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//this function prints 10 strings
 void printTenStrings(const char * text) 
 {
 	int i;
@@ -12,6 +13,7 @@ void printTenStrings(const char * text)
 	}
 }
 
+//start routine function for thread
 void * threadBody(void * param) 
 {
 	printTenStrings("I'm thread");
@@ -21,12 +23,23 @@ int main()
 {
 	pthread_t thread;
 
+	//pthread_create create new thread with params : 
+	//	pointer on struct pthread_t
+	//	pointer on struct pthread_attr_t 
+	//				(if we want to create thread with default attrs, use NULL)
+	//	pointer on start rountine function
+	//	args for function threadBody(if no args use NULL)
 	int code = pthread_create(&thread, NULL, threadBody, NULL);
+	
+	// We have an error if code != 0
 	if (code != 0)
 	{
+		//print error in std::err with function perror
 		perror("Thread create error");
 		exit(0);
 	}
 	printTenStrings("yes");
+	
+	//Use pthread_exit to terminate current thread
 	pthread_exit(NULL);
 }
